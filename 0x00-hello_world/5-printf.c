3fe83739ddc7b79bfbd7fd047d9032b4abc17988
Muhammad Hussein <muhammad.a.mahmoud@outlook.com>
2023-10-05 03:54:07 -0700

    fix

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,10 +2,10 @@
 /**
  * main - entry point
  *
- * return: 0
+ * Return: 0
  */
 int main(void)
 {
-	printf("with power grammar, but the outcome is a piece of art,\n");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
