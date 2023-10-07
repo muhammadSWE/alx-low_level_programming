daaa1bbbeaba1b2e43734f2d1d619571b26488de
Muhammad Hussein <muhammad.a.mahmoud@outlook.com>
2023-10-07 05:06:38 -0700

    fix

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ *main - entry point
+ *
+ *Return: 0 
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0 ; i < 10 ; i++)
+	{
+		if (i == 9)
+		{
+			printf("%d", i);
+
+		
+		}
+		else
+		{
+			printf("%d, ", i);
+		}
+	}
+	printf("\n");
+	return (0);
+}
