5acaa2ea3f3a353a358d6f6bbc081d9b2fc3968b
Muhammad Hussein <muhammad.a.mahmoud@outlook.com>
2023-10-07 04:25:47 -0700

    fix

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+    * main - entry point
+      *
+        * Return: 0
+	  */
+int main(void)
+{
+		char alphabet[52] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+		int i;
+
+	for (i = 0 ; i < 52 ; i++)
+	{
+		putchar(alphabet[i]);
+	}
+
+	putchar('\n');
+	return (0);
+
+}
