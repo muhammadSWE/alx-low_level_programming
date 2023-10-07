2fa796f7dd1f6d8726181cac44c46ae92c85aa11
Muhammad Hussein <muhammad.a.mahmoud@outlook.com>
2023-10-07 04:16:50 -0700

    fix

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -7,14 +7,15 @@
 int main(void)
 {
 	char alphabet[26] = "abcdefghijklmnopqrtsuvwxyz";
-	
+	int i;
+
 	for (i = 0 ; i < 26 ; i++)
 	{
 		putchar(alphabet[i]);
 	}
 
 	putchar('\n');
-	
+
 	return (0);
 
 }
