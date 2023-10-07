7eb65f7734d30b10c949b5e0a99cc4e0bb635b68
Muhammad Hussein <muhammad.a.mahmoud@outlook.com>
2023-10-07 07:21:34 -0700

    fix

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -5,31 +5,31 @@
 *Return: 0
 */
 int main() {
-  int i, j, k, l;
 
-        for (i = 48 ; i < 58 ; i++)
-        {
-                for (j  = 48; j < 57; j++)
-                {
-                        for (k = i; k < 58; k++)
-                        {
-                                for (l = j +1; l < 58; l++)
-                                        {
-                                                putchar(i);
-                                                putchar(j);
-                                                putchar(' ');
-                                                putchar(k);
-                                                putchar(l);
-                                                        if (i != 57 || j != 56 || k != 57 || l != 57)
-                                                                {
-                                                                        putchar(',');
-                                                                        putchar(' ');
-                                                                }
+int i, j;
 
-                                        }
-                        }
-                }
-        }
-        putchar('\n');
-        return (0);
+	for (i = 0; i < 100; i++)
+	{
+		for (j = 0; j < 100; j++)
+		{
+			if (i < j)
+			{
+				putchar((i / 10) + 48);
+				putchar((i % 10) + 48);
+				putchar(' ');
+				putchar((j / 10) + 48);
+				putchar((j % 10) + 48);
+
+				if (i != 98 || j != 99)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+
+		}
+	}
+	putchar('\n');
+	return (0);
+
 }
