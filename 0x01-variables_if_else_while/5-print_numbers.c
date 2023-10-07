4cba85cf1772594bdc47deb7c337ece477b48fd3
Muhammad Hussein <muhammad.a.mahmoud@outlook.com>
2023-10-07 04:34:53 -0700

    fix

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+  *main - entry point
+  *
+  *Return: 0
+  */
+int main(void)
+{
+	int i;
+
+	for (i = 0 ; i < 10 ; i++)
+	{
+		printf(i);
+	}
+	printf('\n');
+	return (0);
+}
