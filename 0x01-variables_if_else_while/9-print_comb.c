05ce2f2835d7f5c19f25c2ec703538d0b4f479ae
Muhammad Hussein <muhammad.a.mahmoud@outlook.com>
2023-10-07 05:46:39 -0700

    fix

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -9,15 +9,14 @@ int main(void)
 	int i;
 
 	for (i = 48 ; i < 58 ; i++)
-	{	
+	{
 		putchar(i);
 
-			if (i < 58)
+			if (i < 57)
 			{
 				putchar(',');
 				putchar(' ');
-			}
-		
+			}	
 	}
 	putchar('\n');
 	return (0);
