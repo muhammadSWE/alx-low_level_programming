e7042d7e5261cf9e3e3ef077ba6382306e84b989
Muhammad Hussein <32427243+muhammadSWE@users.noreply.github.com>
2023-10-11 00:27:39 +0300

    fix

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -10,40 +10,32 @@ void print_times_table(int n)
 	int digit, multiplier, result;
 
 	if (n >= 0 && n <= 15)
-	{
-		for (digit = 0; digit < n + 1; digit++)
+	{	for (digit = 0; digit < n + 1; digit++)
 		{
 			for (multiplier = 0; multiplier < n + 1; multiplier++)
-			{
-				result = digit * multiplier;
+			{	result = digit * multiplier;
 				if (multiplier == 0)
-				{
-					_putchar(result + 48);
-				}
+				{	_putchar(result + 48);	}
 				else if (result / 10 == 0 && multiplier != 0)
-				{
-					_putchar(' ');
+				{	_putchar(' ');
 					_putchar(' ');
 					_putchar(' ');
 					_putchar (result + 48);
 				}
 				else if ((result / 10 > 0) && (result / 10 < 10))
-				{
-					_putchar(' ');
+				{	_putchar(' ');
 					_putchar(' ');
 					_putchar((result / 10) + 48);
 					_putchar((result % 10) + 48);
 				}
 				else
-				{
-					_putchar(' ');
+				{	_putchar(' ');
 					_putchar((result / 100) + 48);
 					_putchar((result / 10 % 10) + 48);
 					_putchar((result % 10) + 48);
 				}
 				if (multiplier != n)
-				{
-					_putchar(',');
+				{	_putchar(',');
 				}
 			} _putchar('\n');
 		}
