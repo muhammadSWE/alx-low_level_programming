e0c101fcb7a028541bc062233f44368212fd76ca
Muhammad Hussein <muhammad.a.mahmoud@outlook.com>
2023-10-10 02:58:40 -0700

    fix

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Return: 0
+ */
+void print_alphabet(void)
+{
+	putchar("abcdefghijklmnopqrstuvwxyz");
+	return;
+}
+int main(void)
+{
+	print_alphabet();
+	return (0);
+}
