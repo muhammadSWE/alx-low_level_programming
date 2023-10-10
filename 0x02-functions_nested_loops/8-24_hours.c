79a4f1f7e8ceac58d95d6c89867b2eafc2ec64ab
Muhammad Hussein <muhammad.a.mahmoud@outlook.com>
2023-10-10 06:45:15 -0700

    fix

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,32 @@
+#include "main.h"
+/**
+ *jack bauer - prints every minute of the day
+ *
+ * Return: void
+ */
+void jack_bauer(void)
+{
+	int i, j, k, l;
+
+	for (i = 48; i < 51; i++)
+	{
+		for (j = 48; j < 58; j++)
+		{
+			if (i + j < 102)
+			{
+				for (k = 48; k < 54; k++)
+				{
+					for (l = 48; l < 58; l++)
+					{
+						_putchar(i);
+						_putchar(j);
+						_putchar(':');
+						_putchar(k);
+						_putchar(l);
+						_putchar('\n');
+					}
+				}
+			}
+		}
+	}
+}
