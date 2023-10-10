66143522c313bdf616c43459693a1e140e8101ce
Muhammad Hussein <32427243+muhammadSWE@users.noreply.github.com>
2023-10-11 00:30:29 +0300

    fix

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -10,7 +10,7 @@ void print_times_table(int n)
 	int digit, multiplier, result;
 
 	if (n >= 0 && n <= 15)
-	{	
+	{
 		for (digit = 0; digit < n + 1; digit++)
 		{
 			for (multiplier = 0; multiplier < n + 1; multiplier++)
