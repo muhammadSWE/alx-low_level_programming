ccc253a938763e9a733fc35a02f0bdc33885b6db
Muhammad Hussein <muhammad.a.mahmoud@outlook.com>
2023-10-10 04:54:31 -0700

    fix

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _abs - prints absolute value of int
+ *@x: int input
+ * Return: int
+ */
+int _abs(int x)
+{
+	if (x < 0)
+	{
+		return (-x);
+	}
+	else
+	{
+		return (x);
+	}
+}
