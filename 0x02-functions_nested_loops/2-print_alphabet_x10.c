8a69c09b58bcf278e96d408b1eeaefced242f019
Muhammad Hussein <muhammad.a.mahmoud@outlook.com>
2023-10-10 04:00:06 -0700

    fix

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <string.h>
+/**
+ * print_alphabet - prints alphabet using _putchar
+ *
+ * Return: void
+ */
+void print_alphabet_x10(void)
+{
+	int i;
+	char alpha[] = "abcdefghijklmnopqrstuvwxyz";
+	int stringlength = strlen(alpha);
+
+	for (j = 0; j < 10; j++)
+	{
+		for (i = 0; i < stringlength; i++)
+		{
+			_putchar(alpha[i]);
+		}
+	_putchar('\n');
+	}
+}
