849fae13b28ae5ff2d22c193154a3966cb21147a
Muhammad Hussein <muhammad.a.mahmoud@outlook.com>
2023-10-10 07:47:11 -0700

    fix

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,43 @@
+#include "main.h"
+/**
+ * times_table - prints 9 times table
+ *
+ * Return: void
+ */
+void times_table(void)
+{
+	int digit, multiplier, result;
+
+	for (digit = 0; digit < 10; digit++)
+	{
+		for (multiplier = 0; multiplier < 10; multiplier++)
+		{
+			result = digit * multiplier;
+
+			if ((result / 10) != 0)
+			{
+				_putchar((result / 10) + 48);
+				_putchar((result % 10) + 48);
+			}
+			else
+			{
+				_putchar((result % 10) + 48);	
+			}
+			if (multiplier != 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+		}
+		_putchar('\n');
+	}
+}
+
+
+
+
+
+
+
+
+}
