3d66955b782ecd3b46d8cb58675da2f67e3902db
Muhammad Hussein <muhammad.a.mahmoud@outlook.com>
2023-10-12 07:31:14 -0700

    fix

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,33 @@
+#include "main.h"
+/**
+ * print_diagonal - draw diagonal line
+ *@n: number of \
+ * Return: void
+ */
+void print_diagonal(int n)
+{
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		int i, j;
+
+		for (i = 0; i < n; i++)
+		{
+			for (j = 0; j < i; j++)
+			{
+				if (j != i - 1 && i != 1)
+				{
+					_putchar(' ');
+				}
+				else
+				{
+					_putchar('\\');
+				}
+		}
+		_putchar('\n');
+	}
+}
