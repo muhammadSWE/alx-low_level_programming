8d47a0cb7fae3235f63908cf5543704d8ba38d2f
Muhammad Hussein <muhammad.a.mahmoud@outlook.com>
2023-10-12 07:57:07 -0700

    Fix

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * print_square - draw a square
+ *@size: square size
+ * Return: void
+ */
+void print_square(int size)
+{
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		int i, j;
+
+		for (i = 0; i < size; i++)
+		{
+			for (j = 0; j < size; j++)
+			{
+				_putchar('#');
+			}
+		_putchar('\n');
+		}
+	}
+}
