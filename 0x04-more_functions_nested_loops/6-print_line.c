d8ec7fbe3b278f45307cdeda9d77d0317ede1f15
Muhammad Hussein <muhammad.a.mahmoud@outlook.com>
2023-10-12 06:34:05 -0700

    fix

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_line - draws a straight line in terminal
+ *@n: number of _ 
+ * Return: void
+ */
+void print_line(int n)
+{
+	int i;
+
+	if (n > 0)
+	{
+		for (i = 0; i < n; i++)
+		{
+			_putchar('_');
+		}
+	}
+	_putchar('\n');
+}
