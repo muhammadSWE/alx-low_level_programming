d103d61f4c37e3b569d0823ec67294cd20d67dd5
Muhammad Hussein <muhammad.a.mahmoud@outlook.com>
2023-10-12 06:52:06 -0700

    fix

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -8,16 +8,16 @@ void print_line(int n)
 {
 	int i;
 
-	if (n > 0)
+	if (n <= 0)
 	{
+		_putchar('\n');
+	}
+	else
+	{	
 		for (i = 0; i < n; i++)
 		{
 			_putchar('_');
 		}
-		_putchar('\n');
-	}
-	else
-	{
 		_putchar('\n');
 	}
 }
