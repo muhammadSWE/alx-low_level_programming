551838f9447537e5e170a79926a6129c49eff3b3
Muhammad Hussein <muhammad.a.mahmoud@outlook.com>
2023-10-16 11:23:12 +0000

    fix

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+  *swap_int - swap two integers
+  *@a: first integer
+  *@b: second integer
+  *
+  *Return: void
+  */
+void swap_int(int *a, int *b)
+{
+	int i;
+	i = *a;
+	*a = *b;
+	*b = i;
+}
