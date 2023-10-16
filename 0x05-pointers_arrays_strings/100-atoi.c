60a6a1ff8183341b16f5ac15c63383626a3526f2
Muhammad Hussein <muhammad.a.mahmoud@outlook.com>
2023-10-16 14:55:24 +0000

    fix

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,32 @@
+#include "main.h"
+/**
+  * _atoi - returns integers in a string
+  * @s: string
+  *
+  * Return: integer
+  */
+int _atoi(char *s)
+{
+	int output = 0;
+
+	int sign = 1;
+
+	while (*s && (*s < '0' || *s > '9'))
+	{
+		++s;
+	}
+
+	if (*s == '-')
+	{
+		sign = -1;
+		++s;
+	}
+
+	while (*s && (*s >= '0' && *s <= '9'))
+	{
+		output = output * 10 + (*s - '0');
+		++s;
+	}
+
+	return (sign * output);
+}
