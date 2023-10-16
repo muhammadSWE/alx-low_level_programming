287acb77f77e6fbe69ea665955aab878743292f3
Muhammad Hussein <muhammad.a.mahmoud@outlook.com>
2023-10-16 15:36:31 +0000

    fix

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - generates random passwords
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int password[100];
+	int i, n;
+
+	int sum = 0;	
+
+	srand(time(NULL));
+
+	for (i = 0; i < 100; i++)
+	{
+		password[i] = rand() % 78;
+		sum += (password[i] + '0');
+		putchar(password[i] + '0');
+		if ((2772 - sum) - '0' < 78)
+		{
+			n = 2772 - sum - '0';
+			sum += n;
+			putchar(n + '0');
+			break;
+		}
+	}
+	return (0);
+}
