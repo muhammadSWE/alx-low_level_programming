526839506f15112f59d27118be4f832eb04e3a2b
Muhammad Hussein <muhammad.a.mahmoud@outlook.com>
2023-10-16 12:37:47 +0000

    fix

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+  *print_rev - prints a string in reverse followed by a new line
+  *@s: string parameter
+  *
+  *Return: void
+  */
+void print_rev(char *s)
+{
+	int i;
+	int len = _strlen(s);
+
+	for (i = len - 1; i >= 0; i--)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
