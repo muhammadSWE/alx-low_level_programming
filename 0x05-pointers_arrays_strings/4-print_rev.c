ba72d942cc217cce6fa7ec1100985ddbb5024a40
Muhammad Hussein <muhammad.a.mahmoud@outlook.com>
2023-10-16 12:40:14 +0000

    fix

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -16,3 +16,20 @@ void print_rev(char *s)
 	}
 	_putchar('\n');
 }
+/**
+  *_strlen - gets string length
+  *@s: string parameter
+  *
+  *Return: string length
+  */
+int _strlen(char *s)
+{
+	int i;
+	int len = 0;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		len++;
+	}
+	return (len);
+}
