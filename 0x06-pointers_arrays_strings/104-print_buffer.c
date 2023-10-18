1483fceeadce064dcc386b63098fe56e68af1307
Muhammad Hussein <muhammad.a.mahmoud@outlook.com>
2023-10-18 21:01:53 +0000

    fix

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -7,32 +7,36 @@
  *
  * Return: void
  */
-void print_buffer(char *b, int size)
-{
-	if (size <= 0)
-	{
+void print_buffer(char *b, int size) {
+	if (size <= 0) {
 		printf("\n");
 		return;
 	}
 
-	for (int i = 0; i < size; i += 10)
-	{
+	int i, j;
+	for (i = 0; i < size; i += 10) {
 		printf("%08x: ", i);
-		for (int j = 0; j < 10; j++)
-		{
-			printf("%s%02x", j % 2 == 0 ? " " : "", i + j < size ? b[i + j] : ' ');
+		for (j = 0; j < 10; j++) {
+			if (j % 2 == 0)
+				printf(" ");
+			if (i + j < size) {
+				printf("%02x", b[i + j]);
+			} else {
+				printf("  ");
+			}
 		}
 		printf(" ");
-		for (int j = 0; j < 10; j++)
-		{
-			char c = '.';
-
-			if (i + j < size && b[i + j] >= 32 && b[i + j] <= 126)
-			{
-				c = b[i + j];
+		for (j = 0; j < 10; j++) {
+			if (i + j < size) {
+				if (b[i + j] >= 32 && b[i + j] <= 126) {
+					printf("%c", b[i + j]);
+				} else {
+					printf(".");
+				}
+			} else {
+				printf(" ");
 			}
-		printf("%c", c);
 		}
-	printf("\n");
+		printf("\n");
 	}
 }
