ce9f5d3b8a0dda2528df1f4af4cb8485ee5fb622
Muhammad Hussein <muhammad.a.mahmoud@outlook.com>
2023-10-18 13:03:48 +0000

    fix

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+  * *cap_string - capitalizes all words of a string
+  *@*s: string
+  *
+  *Return: capitalized string
+  */
+char *cap_string(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] == '\n' || s[i] == ' ')
+		{
+			if (s[i + 1] >= 97 && s[i + 1] <= 122)
+			{
+				s[i + 1] -= 32;
+			}
+		}
+	}
+	return (s);
+}
