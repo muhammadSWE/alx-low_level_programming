fb8c45562ee0dac43c369a4176cb6db667419b82
Muhammad Hussein <muhammad.a.mahmoud@outlook.com>
2023-10-18 12:22:21 +0000

    fix

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+  * *_strcmp  - compare two strings
+  *@*s1: string parameter
+  *@*s2: string parameter
+  *
+  *Return: int result
+  */
+int _strcmp(char *s1, char *s2)
+{
+	int i;
+	int output;
+
+	for (i = 0; s1[i] != '\0' && s2[i] != '\0'; i++)
+	{
+		if (s1[i] != s2[i])
+		{
+			output = s1[i] - s2[i];
+			return (output);
+			break;
+		}
+		else
+		{
+			return (0);
+		}
+	}
+}
