e92c0a1cc48e8e007855e523b11ff35b2f4cad16
Muhammad Hussein <muhammad.a.mahmoud@outlook.com>
2023-10-18 18:10:32 +0000

    fix

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -9,7 +9,8 @@
 char *_strcat(char *dest, char *src)
 {
 	int i;
-	int j = 0;
+
+	int j;
 
 	for (i = 0; dest[i] != '\0'; i++);
 	for (j = 0; src[j] != '\0'; j++)
@@ -17,10 +18,6 @@ char *_strcat(char *dest, char *src)
 		dest[i] = src[j];
 		i++;
 	}
-<<<<<<< HEAD
-		
-=======
->>>>>>> 4f64471f5e2a8551b909f717b22e3572a23ec13b
 	dest[i] = '\0';
 	return (dest);
 }
