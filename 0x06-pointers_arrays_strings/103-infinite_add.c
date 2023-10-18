d0efcd54fb56bdf0756dfc41b00ed078804546cc
Muhammad Hussein <muhammad.a.mahmoud@outlook.com>
2023-10-18 20:23:14 +0000

    fix

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,13 +1,40 @@
 #include "main.h"
+
 /**
- * *infinite_add - add two numbers
- *@n1: first number
- *@n2: second number
- *@r: result
- *@size_r: buffer size
- *
- * Return: sum of two numbers
+ * rev_string - reverse array
+ * @n: integer params
+ * Return: 0
  */
+
+void rev_string(char *n)
+{
+	int i = 0;
+	int j = 0;
+	char temp;
+
+	while (*(n + i) != '\0')
+	{
+		i++;
+	}
+	i--;
+
+	for (j = 0; j < i; j++, i--)
+	{
+		temp = *(n + j);
+		*(n + j) = *(n + i);
+		*(n + i) = temp;
+	}
+}
+
+/**
+ * infinite_add - add 2 numbers together
+ * @n1: text representation of 1st number to add
+ * @n2: text representation of 2nd number to add
+ * @r: pointer to buffer
+ * @size_r: buffer size
+ * Return: pointer to calling function
+ */
+
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
 	int overflow = 0, i = 0, j = 0, digits = 0;
@@ -48,29 +75,4 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 	*(r + digits) = '\0';
 	rev_string(r);
 	return (r);
-}
-/**
-  *rev_string - reverse string
-  *@s: string parameter
-  *
-  *Return: void
-  */
-void rev_string(char *s)
-{
-	int i = 0;
-	int j = 0;
-	char temp;
-
-	while (*(s + i) != '\0')
-	{
-		i++;
-	}
-	i--;
-
-	for (j = 0; j < i; j++, i--)
-	{
-		temp = *(s + j);
-		*(s + j) = *(s + i);
-		*(s + i) = temp;
-	}
 }
