5fd069ea064c6e5fbaaf431f42017369f30ff74c
Muhammad Hussein <muhammad.a.mahmoud@outlook.com>
2023-10-18 11:33:41 +0000

    fix

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,40 @@
+#include "main.h"
+/**
+  * *_strcat  - concatenates two strings
+  *@*dest: string parameter
+  *@*src: string parameter
+  *
+  *Return: concatenated string dest
+  */
+char *_strcat(char *dest, char *src)
+{
+	int lenDest = _strlen(dest);
+	int lenSrc = _strlen(src);
+	int i;
+	int j = 0;
+
+	for (i = lenDest; i < lenDest + lenSrc; i++)
+	{
+		dest[i] = src[j];
+		j++;
+	}
+	dest[lenDest + lenSrc] = '\0';
+	return (dest);
+}
+/**
+  *_strlen - gets string length
+  *@s: string parameter
+  *
+  *Return: string length
+  */
+int _strlen(char *s)
+{
+	int i;
+	int len = 0;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		len++;
+	}
+	return (len);
+}
