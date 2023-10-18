6268426cd065b02b42dde90d7f594cc1420daa83
Muhammad Hussein <muhammad.a.mahmoud@outlook.com>
2023-10-18 20:19:54 +0000

    fix

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -10,32 +10,43 @@
  */
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-	int overflow = 0, digits = 0;
-	int len1 = strlen(n1), len2 = strlen(n2);
+	int overflow = 0, i = 0, j = 0, digits = 0;
+	int val1 = 0, val2 = 0, temp_tot = 0;
 
-	if (len1 >= size_r || len2 >= size_r)
-	return (0);
-
-	for (int i = max_len - 1; i >= 0 || overflow == 1; i--, digits++)
-	{
-		int val1 = (i < len1) ? n1[i] - '0' : 0;
-		int val2 = (i < len2) ? n2[i] - '0' : 0;
-
-		int temp_tot = val1 + val2 + overflow;
-
-		overflow = (temp_tot >= 10) ? 1 : 0;
-
-		if (digits >= (size_r - 1))
+	while (*(n1 + i) != '\0')
+		i++;
+	while (*(n2 + j) != '\0')
+		j++;
+	i--;
+	j--;
+	if (j >= size_r || i >= size_r)
 		return (0);
-
-		r[digits] = (temp_tot % 10) + '0';
+	while (j >= 0 || i >= 0 || overflow == 1)
+	{
+		if (i < 0)
+			val1 = 0;
+		else
+			val1 = *(n1 + i) - '0';
+		if (j < 0)
+			val2 = 0;
+		else
+			val2 = *(n2 + j) - '0';
+		temp_tot = val1 + val2 + overflow;
+		if (temp_tot >= 10)
+			overflow = 1;
+		else
+			overflow = 0;
+		if (digits >= (size_r - 1))
+			return (0);
+		*(r + digits) = (temp_tot % 10) + '0';
+		digits++;
+		j--;
+		i--;
 	}
 	if (digits == size_r)
 		return (0);
-
-	r[digits] = '\0';
+	*(r + digits) = '\0';
 	rev_string(r);
-
 	return (r);
 }
 /**
