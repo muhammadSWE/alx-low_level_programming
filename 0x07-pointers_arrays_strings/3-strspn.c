e3bbb3e8814a0bb8fb69865f84833eaca3ffe984
Muhammad Hussein <muhammad.a.mahmoud@outlook.com>
2023-10-23 09:06:33 +0000

    fix

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * *_strspn - gets length of a prefix substring
+ *@s: string 1
+ *@accept: string 2
+ *
+ *Return: pointer to first occurrence of c
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	int i, j;
+	unsigned int l = 0;
+
+	for (i = 0; accept[i] != '\0'; i++)
+	{
+		for (j = 0; accept[j] != '\0'; j++)
+		{
+			if (s[i] == accept[j])
+			{
+				l++;
+				break;
+			}
+		}
+	}
+	return (l);
+}
