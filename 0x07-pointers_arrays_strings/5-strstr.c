2c0cd0fd282fe9bd153bf53932bba82093499457
Muhammad Hussein <muhammad.a.mahmoud@outlook.com>
2023-10-23 10:29:26 +0000

    fix

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -10,15 +10,15 @@ char *_strstr(char *haystack, char *needle)
 {
 	int i, j;
 
-	for (i = 0; haystack[i] != '\0'; i++)
+	for (i = 0; haystack[i]; i++)
 	{
-		for (j = 0; needle[j] != '\0'; j++)
+		for (j = 0; needle[j]; j++)
 		{
 			if (haystack[i + j] != needle[j])
 			{
 				break;
 			}
-			if (!needle[j + 1])
+			if (needle[j] == '\0')
 			{
 				return (haystack + i);
 			}
