ace1422eafc57c52857ed6da0aef3b3b1bc282b7
Muhammad Hussein <muhammad.a.mahmoud@outlook.com>
2023-10-23 11:02:01 +0000

    fix

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ *print_chessboard - prints the chessboard
+ *@a: 2D array of size 8x8
+ *
+ *Return: void
+ */
+char print_chessboard(char (*a)[8])
+{
+	int i, j;
+
+	for (i = 0; i < 8; i++)
+	{
+		for (j = 0; j < 8; j++)
+		{
+			_putchar(a[i][j]);
+
+			if (j == 7 && i != 7)
+			{
+				_putchar('\n');
+			}
+		}
+	}
+}
