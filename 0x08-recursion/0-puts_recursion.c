89d776547bfd32986207bad0ec24ce12263e2232
Muhammad Hussein <muhammad.a.mahmoud@outlook.com>
2023-10-25 10:00:06 +0000

    fix

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -7,12 +7,14 @@
   */
 void _puts_recursion(char *s)
 {
-	int i = 0;
-
-	while (s[i] == '\0')
+	if (*s == '\0')
 	{
-		_putchar(s[i]);
-		i++;
+		_putchar('\n');
+		return;
 	}
-	_putchar('\n');
+	else
+	{
+		_putchar(*s);
+		_puts_recursion(*s + 1);
+	}
 }
