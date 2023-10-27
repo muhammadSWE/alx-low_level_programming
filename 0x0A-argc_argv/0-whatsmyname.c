6ea959109673948835a13917009c9bfec9724ae4
Muhammad Hussein <muhammad.a.mahmoud@outlook.com>
2023-10-27 20:01:31 +0000

    fix

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,11 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ *printname - prints name followed by a new line
+ *
+ *Return: void
+ */
+void printname(void)
+{
+	printf("%s", argv[0]);
+}
---
+++ 0x0A-argc_argv/main.h
@@ -0,0 +1,1 @@
+void printname(void);
