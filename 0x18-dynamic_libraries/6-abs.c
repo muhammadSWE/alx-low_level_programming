18f8acc3cd9848642d2879f6adbcc81205cb0f12
Muhammad Hussein <muhammad.a.mahmoud@outlook.com>
2024-01-15 23:20:45 +0200

    insert prototypes

--- 0x18-dynamic_libraries/6-abs.c
+++ 0x18-dynamic_libraries/6-abs.c
@@ -1,3 +1,16 @@
 #include "main.h"
-int _abs(int n)
-{}
+/**
+ *_abs - return deppend of sign of n
+ *
+ *Return: 0
+ *@c: number for evaluate
+ */
+int _abs(int c)
+{
+	if (c < 0)
+	{
+		return (-c);
+	}
+	else
+		return (c);
+}
